34074335b0b1bcd15a7d18c5811a51d21f699753
吕振斌 <lvzhenbin@hotmail.com>
2015-10-11 18:54:19 +0800

    refactor CustomerInfoFragment code and modify setting_about layout

---
+++ app/src/main/java/com/lzb/oa/service/CustomerService.java
@@ -0,0 +1,80 @@
+package com.lzb.oa.service;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.lzb.oa.commons.Constant;
+import com.lzb.oa.service.response.ErrorResponse;
+import com.lzb.oa.ui.adapter.CustomerInfoAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by lvzhenbin on 2015/10/11.
+ */
+public class CustomerService {
+
+    public static CustomerService instance;
+
+    public static CustomerService getInstance() {
+        if (instance == null) {
+            instance = new CustomerService();
+        }
+        return instance;
+    }
+
+    /**
+     * 获取客户信息
+     * 
+     * @param context
+     *            getActivity()
+     * @param customerInfoAdapter
+     *            客户信息适配器
+     * @param lvCustomerInfo
+     *            显示客户信息列表的ListViewa
+     */
+    public void getCustomerInfo(Context context,
+            final CustomerInfoAdapter customerInfoAdapter,
+            final ListView lvCustomerInfo) {
+        String CUSTOMER_INFO_URL = Constant.URL + "customer_info.json";
+        final ArrayList<HashMap<String, Object>> listCustomerInfo = new ArrayList<>();
+        RequestQueue mQueue = Volley.newRequestQueue(context);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                CUSTOMER_INFO_URL, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.e("jsonArray", response.toString());
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject json = new JSONObject(response
+                                        .get(i).toString());
+                                HashMap<String, Object> map = new HashMap<>();
+                                map.put("roomer_no", json.get("roomer_no"));
+                                map.put("roomer_name", json.get("roomer_name"));
+                                map.put("roomer_phone_no",
+                                        json.get("roomer_phone_no"));
+                                map.put("roomer_date", json.get("roomer_date"));
+                                listCustomerInfo.add(map);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        customerInfoAdapter.setItemList(listCustomerInfo);
+                        customerInfoAdapter.notifyDataSetChanged();
+                        lvCustomerInfo.setAdapter(customerInfoAdapter);
+                    }
+                }, new ErrorResponse(context));
+        mQueue.add(jsonArrayRequest);
+    }
+}
--- app/src/main/java/com/lzb/oa/ui/customer/CustomerInfoFragment.java
+++ app/src/main/java/com/lzb/oa/ui/customer/CustomerInfoFragment.java
@@ -2,36 +2,19 @@ package com.lzb.oa.ui.customer;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response.ErrorListener;
-import com.android.volley.Response.Listener;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
 import com.lzb.oa.R;
-import com.lzb.oa.commons.Constant;
+import com.lzb.oa.service.CustomerService;
 import com.lzb.oa.ui.adapter.CustomerInfoAdapter;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class CustomerInfoFragment extends Fragment {
 
-    private final static String CUSTOMER_INFO_URL = Constant.URL
-            + "customer_info.json";
 
     private ListView lvCustomerInfo;
-    private RequestQueue mQueue = null;
     private CustomerInfoAdapter customerInfoAdapter;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,7 +33,8 @@ public class CustomerInfoFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        getCustomerInfo();
+        CustomerService.getInstance().getCustomerInfo(getActivity(),
+                customerInfoAdapter, lvCustomerInfo);
     }
 
     // 初始化ListView控件
@@ -60,45 +44,5 @@ public class CustomerInfoFragment extends Fragment {
         customerInfoAdapter = new CustomerInfoAdapter(getActivity());
     }
 
-    // 从服务器获取信息
-    private void getCustomerInfo() {
-        final ArrayList<HashMap<String, Object>> listCustomerInfo = new ArrayList<HashMap<String, Object>>();
-        mQueue = Volley.newRequestQueue(getActivity());
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
-                CUSTOMER_INFO_URL, new Listener<JSONArray>() {
-
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        Log.e("jsonArray", response.toString());
-                        for (int i = 0; i < response.length(); i++) {
-                            try {
-                                JSONObject json = new JSONObject(response
-                                        .get(i).toString());
-
-                                HashMap<String, Object> map = new HashMap<String, Object>();
-                                map.put("roomer_no", json.get("roomer_no"));
-                                map.put("roomer_name", json.get("roomer_name"));
-                                map.put("roomer_phone_no",
-                                        json.get("roomer_phone_no"));
-                                map.put("roomer_date", json.get("roomer_date"));
-                                listCustomerInfo.add(map);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        // 将list集合中的数据传入到自定义的adapter中
-                        customerInfoAdapter.setItemList(listCustomerInfo);
-                        customerInfoAdapter.notifyDataSetChanged();
-                        lvCustomerInfo.setAdapter(customerInfoAdapter);
-                    }
-                }, new ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                });
-        mQueue.add(jsonArrayRequest);
-    }
 
 }
